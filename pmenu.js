8525d2a9113ce4b1e69a2c4fb7fe7e944104597c
thesujaljaiswal <108607135+thesujaljaiswal@users.noreply.github.com>
2022-09-01 01:07:42 +0530

    files uploadation

---
+++ pmenu.js
@@ -0,0 +1,196 @@
+// white malai peda
+function whitemalaipeda2() {
+    document.getElementById("whitemalaipeda").src = "whitemalaipeda2.jpg";
+}
+function whitemalaipeda1() {
+    document.getElementById("whitemalaipeda").src = "whitemalaipeda1.jpg";
+;}
+// yellow malai peda
+function yellowmalaipeda2() {
+    document.getElementById("yellowmalaipeda").src = "yellowmalaipeda2.jpg";
+}
+function yellowmalaipeda1() {
+    document.getElementById("yellowmalaipeda").src = "yellowmalaipeda1.jpg";
+}
+// cream milk cake
+function creammilkcake2() {
+    document.getElementById("creammilkcake").src = "creammilkcake2.jpg";
+}
+function creammilkcake1() {
+    document.getElementById("creammilkcake").src = "creammilkcake1.jpg";
+}
+// milk cake
+function milkcaek2() {
+    document.getElementById("milkcake").src = "milkcake2.jpg";
+}
+function milkcake1() {
+    document.getElementById("milkcake").src = "milkcake1.jpg";
+}
+// malai barfi
+function malaibarfi2() {
+    document.getElementById("malaibarfi").src = "malaibarfi2.jpg";
+}
+function malaibarfi1() {
+    document.getElementById("malaibarfi").src = "malaibarfi1.jpg";
+}
+// mava roll
+function mavaroll2() {
+    document.getElementById("mavaroll").src = "mavaroll2.jpg";
+}
+function mavaroll1() {
+    document.getElementById("mavaroll").src = "mavaroll1.jpg";
+}
+// white sada peda
+function whitepeda2() {
+    document.getElementById("whitepeda").src = "whitepeda2.jpg";
+}
+function whitepeda1() {
+    document.getElementById("whitepeda").src = "whitepeda1.jpg";
+}
+// yellow sada peda
+function yellowpeda2() {
+    document.getElementById("yellowpeda").src = "yellowpeda2.jpg";
+}
+function yellowpeda1() {
+    document.getElementById("yellowpeda").src = "yellowpeda1.jpg";
+}
+// motichoor laddu
+function motichoorladdu2() {
+    document.getElementById("motichoorladdu").src = "motichoorladdu2.jpg";
+}
+function motichoorladdu1() {
+    document.getElementById("motichoorladdu").src = "motichoorladdu1.jpg";
+}
+// boondi laddu
+function boondiladdu2() {
+    document.getElementById("boondiladdu").src = "boondiladdu2.jpg";
+}
+function boondiladdu1() {
+    document.getElementById("boondiladdu").src = "boondiladdu1.jpg";
+}
+// besan laddu
+function besanladdu2() {
+    document.getElementById("besanladdu").src = "besanladdu2.jpg";
+}
+function besanladdu1() {
+    document.getElementById("besanladdu").src = "besanladdu1.jpg";
+}
+// badami halwa
+function badamihalwa2() {
+    document.getElementById("badamihalwa").src = "badamihalwa2.jpg";
+}
+function badamihalwa1() {
+    document.getElementById("badamihalwa").src = "badamihalwa1.jpg";
+}
+// mahsoor pak
+function mahsoorpak2() {
+    document.getElementById("mahsoorpak").src = "mahsoorpak2.jpg";
+}
+function mahsoorpak1() {
+    document.getElementById("mahsoorpak").src = "mahsoorpak1.jpg";
+}
+// khaja
+function khaja2() {
+    document.getElementById("khaja").src = "khaja2.jpg";
+}
+function khaja1() {
+    document.getElementById("khaja").src = "khaja1.jpg";
+}
+// mahim halwa
+function mahimhalwa2() {
+    document.getElementById("mahimhalwa").src = "mahimhalwa2.jpg";
+}
+function mahimhalwa1() {
+    document.getElementById("mahimhalwa").src = "mahimhalwa1.jpg";
+}
+// jalebi
+function jalebi2() {
+    document.getElementById("jalebi").src = "jalebi2.jpg";
+}
+function jalebi1() {
+    document.getElementById("jalebi").src = "jalebi1.jpg";
+}
+// boondi
+function boondi2() {
+    document.getElementById("boondi").src = "boondi2.jpg";
+}
+function boondi1() {
+    document.getElementById("boondi").src = "boondi1.jpg";
+}
+// modak
+function modak1() {
+    document.getElementById("modak").src = "modak1.jpg";
+}
+function modak2() {
+    document.getElementById("modak").src = "modak2.jpg";
+}
+// samosa
+function samosa2() {
+    document.getElementById("samosa").src = "samosa2.jpg";
+}
+function samosa1() {
+    document.getElementById("samosa").src = "samosa1.jpg";
+}
+// kachori
+function kachori2() {
+    document.getElementById("kachori").src = "kachori2.jpg";
+}
+function kachori1() {
+    document.getElementById("kachori").src = "kachori1.jpg";
+}
+// lassi
+function lassi2() {
+    document.getElementById("lassi").src = "lassi2.jpg";
+}
+function lassi1() {
+    document.getElementById("lassi").src = "lassi1.jpg";
+}
+// meetha samosa
+function meethasamosa2() {
+    document.getElementById("meethasamosa").src = "meethasamosa2.jpg";
+}
+function meethasamosa1() {
+    document.getElementById("meethasamosa").src = "meethasamosa1.jpg";
+}
+// fafda
+function fafda2() {
+    document.getElementById("fafda").src = "fafda2.jpg";
+}
+function fafda1() {
+    document.getElementById("fafda").src = "fafda1.jpg";
+}
+// ussal pav
+function ussalpav2() {
+    document.getElementById("ussalpav").src = "ussalpav2.jpg";
+}
+function ussalpav1() {
+    document.getElementById("ussalpav").src = "ussalpav.jpg";
+}
+// besan sev
+function besansev2() {
+    document.getElementById("besansev").src = "besansev2.jpg";
+}
+function besansev1() {
+    document.getElementById("besansev").src = "besansev1.jpg";
+}
+// gathiya
+function gathiya2() {
+    document.getElementById("gathiya").src = "gathiya2.jpg";
+}
+function gathiya1() {
+    document.getElementById("gathiya").src = "gathiya1.jpg";
+}
+// teekhagathiya
+function teekhagathiya2() {
+    document.getElementById("teekhagathiya").src = "teekhagathiya2.jpg";
+}
+function teekhagathiya1() {
+    document.getElementById("teekhagathiya").src = "teekhagathiya1.jpg";
+}
+// namkeenpapdi
+function namkeenpapdi2() {
+    document.getElementById("namkeenpapdi").src = "namkeenpapdi2.jpg";
+}
+function namkeenpapdi1() {
+    document.getElementById("namkeenpapdi").src = "namkeenpapdi1.jpg";
+}
